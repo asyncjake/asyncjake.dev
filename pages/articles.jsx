57ee993fc686ff31c938ac40380db658176b9aaf
jakethedev <jak@aero>
2024-10-19 23:06:36 -0600

    Import of itsnitinr/vscode-portfolio baseline

---
+++ pages/articles.jsx
@@ -0,0 +1,47 @@
+import ArticleCard from '../components/ArticleCard';
+import styles from '../styles/ArticlesPage.module.css';
+
+const ArticlesPage = ({ articles }) => {
+  return (
+    <>
+      <h3>
+        Recent Posts from{' '}
+        <a
+          href="https://dev.to/itsnitinr"
+          target="_blank"
+          rel="noopener"
+          className={styles.underline}
+        >
+          dev.to
+        </a>
+      </h3>
+      <div className={styles.container}>
+        {/* TODO: article structure refactor, use next content? mdc? */}
+        {articles.map((article) => (
+          <ArticleCard key={article.id} article={article} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  const res = await fetch(
+    'https://dev.to/api/articles/me/published?per_page=6',
+    {
+      headers: {
+        'api-key': process.env.DEV_TO_API_KEY,
+      },
+    }
+  );
+
+  {/* TODO: article structure refactor, use next content? mdc? */ }
+  const data = await res.json();
+
+  return {
+    props: { title: 'Articles', articles: data },
+    revalidate: 60,
+  };
+}
+
+export default ArticlesPage;
